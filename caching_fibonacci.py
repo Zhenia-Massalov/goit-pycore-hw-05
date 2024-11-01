000a471163286eb0e16b883300d04a616ec044b5
Yevhenii Massalov <111449973+Zhenia-Massalov@users.noreply.github.com>
2024-11-02 01:17:35 +0200

    Add files via upload

---
+++ caching_fibonacci.py
@@ -0,0 +1,21 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n in cache:
+            return cache[n]
+        
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        result = fibonacci(n - 1) + fibonacci(n - 2)
+        cache[n] = result  
+        return result
+
+    return fibonacci 
+
+fib = caching_fibonacci()
+print(fib(10))  
+print(fib(15))  
